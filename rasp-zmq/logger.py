3a985e7dbbb01c57d67a797e6a4837c2844f68a9
Jean-Martin <git@jmartin.ca>
2013-12-13 17:16:27 -0800

    wip

---
+++ rasp-zmq/logger.py
@@ -0,0 +1,22 @@
+import logging
+
+logger = logging.getLogger('GPIO')
+logger.setLevel(logging.DEBUG)
+
+console_output = logging.StreamHandler()
+console_output.setLevel(logging.DEBUG)
+
+file_output = logging.FileHandler(config['log_path'])
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_output.setFormatter(formatter)
+console_output.setFormatter(formatter)
+
+logger.addHandler(console_output)
+logger.addHandler(file_output)
+
+# logger.debug('debug message')
+# logger.info('info message')
+# logger.warn('warn message')
+# logger.error('error message')
+# logger.critical('critical message')
